585b1ea37a66f8dd7e241f2f8a95cad2d4813d12
sandeepin <sandeepin@qq.com>
2020-02-20 23:22:54 +0800

    动态生成tree

--- src/main/java/me/jfz/reader/RssData.java
+++ src/main/java/me/jfz/reader/RssData.java
@@ -1,5 +1,7 @@
 package me.jfz.reader;
 
+import me.jfz.reader.model.SubscibeModel;
+
 import com.rometools.rome.feed.synd.SyndFeed;
 
 import java.util.HashMap;
@@ -16,15 +18,42 @@ public class RssData {
     public static Map<String, String> nameAndUrl = new HashMap<>(128);
 
     static {
-        nameAndUrl.put("football", "https://fulibus.net/feed");
-        nameAndUrl.put("hockey", "http://www.ruanyifeng.com/blog/atom.xml");
-        nameAndUrl.put("hot dogs", "https://www.ghpym.com/feed");
-        nameAndUrl.put("pizza", "http://poi.ooo/index.xml");
-        nameAndUrl.put("ravioli", "https://www.landiannews.com/feed");
-        nameAndUrl.put("bananas", "http://feed.cnblogs.com/blog/u/72021/rss/");
+        nameAndUrl.put("福利吧", "https://fulibus.net/feed");
+        nameAndUrl.put("阮一峰", "http://www.ruanyifeng.com/blog/atom.xml");
+        nameAndUrl.put("ghpym", "https://www.ghpym.com/feed");
+        nameAndUrl.put("异想萌域", "http://poi.ooo/index.xml");
+        nameAndUrl.put("landiannews", "https://www.landiannews.com/feed");
+        nameAndUrl.put("Sandeepin博客园", "http://feed.cnblogs.com/blog/u/72021/rss/");
     }
 
-    public static Map<String, String> titleAndContentMap = new HashMap<>(128);
-
     public static Map<String, SyndFeed> nameAndSyndFeedMap = new HashMap<>(128);
+
+    public static Map<String, SubscibeModel> idAndSubscibeModelMap = new HashMap<>(128);
+
+    static {
+        SubscibeModel subscibeModelType1 = new SubscibeModel("我的博客", "", 0, "root");
+        SubscibeModel subscibeModelType2 = new SubscibeModel("科技IT", "", 0, "root");
+        SubscibeModel subscibeModelType3 = new SubscibeModel("其它收藏", "", 0, "root");
+
+        SubscibeModel subscibeModel1 = new SubscibeModel("Sandeepin博客园", "http://feed.cnblogs.com/blog/u/72021/rss/", 1, subscibeModelType1.getId());
+        SubscibeModel subscibeModel2 = new SubscibeModel("异想萌域", "http://poi.ooo/index.xml", 1, subscibeModelType1.getId());
+
+        SubscibeModel subscibeModel3 = new SubscibeModel("landiannews", "https://www.landiannews.com/feed", 1, subscibeModelType2.getId());
+        SubscibeModel subscibeModel4 = new SubscibeModel("ghpym", "https://www.ghpym.com/feed", 1, subscibeModelType2.getId());
+        SubscibeModel subscibeModel5 = new SubscibeModel("阮一峰", "http://www.ruanyifeng.com/blog/atom.xml", 1, subscibeModelType2.getId());
+
+        SubscibeModel subscibeModel6 = new SubscibeModel("福利吧", "https://fulibus.net/feed", 1, subscibeModelType3.getId());
+
+        idAndSubscibeModelMap.putIfAbsent(subscibeModelType1.getId(), subscibeModelType1);
+        idAndSubscibeModelMap.putIfAbsent(subscibeModelType2.getId(), subscibeModelType2);
+        idAndSubscibeModelMap.putIfAbsent(subscibeModelType3.getId(), subscibeModelType3);
+
+        idAndSubscibeModelMap.putIfAbsent(subscibeModel1.getId(), subscibeModel1);
+        idAndSubscibeModelMap.putIfAbsent(subscibeModel2.getId(), subscibeModel2);
+        idAndSubscibeModelMap.putIfAbsent(subscibeModel3.getId(), subscibeModel3);
+        idAndSubscibeModelMap.putIfAbsent(subscibeModel4.getId(), subscibeModel4);
+        idAndSubscibeModelMap.putIfAbsent(subscibeModel5.getId(), subscibeModel5);
+        idAndSubscibeModelMap.putIfAbsent(subscibeModel6.getId(), subscibeModel6);
+    }
+
 }
---
+++ src/main/java/me/jfz/reader/model/SubscibeModel.java
@@ -0,0 +1,101 @@
+package me.jfz.reader.model;
+
+import java.util.Objects;
+import java.util.UUID;
+
+/**
+ * 描述
+ *
+ * @author Sandeepin
+ * @since 2020/2/20 0020
+ */
+public class SubscibeModel {
+    private String id;
+
+    private String name;
+
+    private String url;
+
+    // -1根 0分类 1订阅源
+    private int type;
+
+    private String preNode;
+
+    public SubscibeModel(String id, String name, String url, int type, String preNode) {
+        this.id = id;
+        this.name = name;
+        this.url = url;
+        this.type = type;
+        this.preNode = preNode;
+    }
+
+    public SubscibeModel(String name, String url, int type, String preNode) {
+        this.id = UUID.randomUUID().toString();
+        this.name = name;
+        this.url = url;
+        this.type = type;
+        this.preNode = preNode;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getPreNode() {
+        return preNode;
+    }
+
+    public void setPreNode(String preNode) {
+        this.preNode = preNode;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        SubscibeModel that = (SubscibeModel) o;
+        return Objects.equals(id, that.id) && Objects.equals(url, that.url);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, url);
+    }
+}
